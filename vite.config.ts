4c390c9320d3f85633392749966c8cfaeda03342
ilkan <ilkan89@gmail.com>
2025-06-30 11:42:25 +0300

    Fix PWA Build Configuration Error

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
@@ -38,10 +38,7 @@ export default defineConfig({
               cacheName: 'google-fonts-cache',
               expiration: {
                 maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-              },
-              cacheKeyWillBeUsed: ({ request }) => {
-                return `${request.url}`
+                maxAgeSeconds: 60 * 60 * 24 * 365
               }
             }
           }
